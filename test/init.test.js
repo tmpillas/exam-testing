c032b32b3bd7467a834f5101e067c62f6e0a67d5
tmpillas <tmpillas@ece.auth.gr>
2025-02-02 23:14:03 +0200

    Final preparation

---
+++ test/init.test.js
@@ -0,0 +1,77 @@
+const test = require('ava');
+const got = require('got');
+const app = require('../index.js');
+const http = require('http');
+
+
+
+test.before(async (t) => {
+    t.context.server = http.createServer(app);
+    // const server = t.context.server.listen(0);
+    // const { port } = server.address();
+    t.context.got = got.extend({ prefixUrl: `http://localhost:8080` });
+    console.log('Server started');
+});
+// After the tests
+test.after.always((t) => {
+    t.context.server.close();
+    console.log('Server closed');
+});
+
+
+// Test case for getting a book by valid ID
+test("GET /book/{id} - Should return a book for a valid ID", async (t) => {
+    const {body, statusCode} = await t.context.got("books");
+    t.is(statusCode, 200, "Response status should be 200");
+});
+
+test("GET authors/{authorId} endpoint returns valid JSON content-type header", async (t) => {
+	const { headers, statusCode } = await t.context.got("authors/0");
+	t.is(statusCode, 200);                   // The status code should be 200
+	t.is(headers["content-type"], "application/json"); // The content-type should be JSON
+});
+
+
+test("GET /book/{id} - Shdfdsfould return a book for a valid ID", async (t) => {
+    const {body, statusCode} = await t.context.got("authors/0");
+    t.is(statusCode, 200, "Response status should be 200");
+});
+
+test("DELETE /book/{id} - Should return a book for a valid ID", async (t) => {
+    const {body, statusCode} = await t.context.got.delete("books/0");
+    t.is(statusCode, 200, "Response status should be 200");
+});
+
+test("autorIdPut", async (t) => {
+    const {body, statusCode} = await t.context.got.put("authors/0", {json: {name: "no"}});
+    t.is(statusCode, 200, "Response status should be 200");
+  });
+  
+
+  const Bookdata = {
+    "title": "string",
+    "author_id": 0,
+    "category_id": 0,
+    "id": 0,
+    "published_year": 0
+  };
+  
+  test("BookPost", async (t) => {
+    const{headers, statusCode} = await t.context.got.post(`books`, {json: Bookdata, responseType: 'json'});
+    t.is(statusCode, 200, "Response status should be 200");
+    t.is(headers["content-type"], "application/json"); // The content-type should be JSON
+  });
+
+  test("GET categories/{categoryId} endpoint returns valid JSON content-type header", async (t) => {
+        const categoryId = 1;
+        const {headers, statusCode} = await t.context.got(`categories/${categoryId}`);
+        t.is(statusCode, 200);                   // The status code should be 200
+        t.is(headers["content-type"], "application/json"); // The content-type should be JSON
+  });
+
+  test("DELETE categories/{categoryId} endpoint returns valid JSON content-type header", async (t) => {
+    const categoryId = 1;
+    const{headers, statusCode} = await t.context.got.delete(`categories/${categoryId}`);
+    t.is(statusCode, 200);                   // The status code should be 200
+    t.is(headers["content-type"], "application/json"); // The content-type should be JSON
+});
